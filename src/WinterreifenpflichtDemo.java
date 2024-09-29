bed99329a65cf8e702bc48714cbf928ad7578ec7
Markus Fritz <52305084@edu.campus02.at>
2024-09-29 22:14:56 +0200

    Übung 2 gelöst, vielleicht später elegantere Lösung finden

--- src/WinterreifenpflichtDemo.java
+++ src/WinterreifenpflichtDemo.java
@@ -1,3 +1,36 @@
 public class WinterreifenpflichtDemo {
+    public static void main(String[] args) {
 
+        if (isWinterreifenPflicht(5,false)){
+            System.out.println("Bitte Winterreifen verwenden");
+        } else {
+            System.out.println("Winterreifen sind nicht erforderlich");
+        }
+        if (isWinterreifenPflicht(3,false)){
+            System.out.println("Bitte Winterreifen verwenden");
+        } else {
+            System.out.println("Winterreifen sind nicht erforderlich");
+        }
+        if (isWinterreifenPflicht(8,true)){
+            System.out.println("Bitte Winterreifen verwenden");
+        } else {
+            System.out.println("Winterreifen sind nicht erforderlich");
+        }
+        if (isWinterreifenPflicht(12,false)){
+            System.out.println("Bitte Winterreifen verwenden");
+        } else {
+            System.out.println("Winterreifen sind nicht erforderlich");
+        }
+
+    }
+
+    public static boolean isWinterreifenPflicht (int temperatur, boolean rutschigeFahrbahn) {
+        if (temperatur < 10 && rutschigeFahrbahn){
+            return true;
+        } else if (temperatur < 4) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
