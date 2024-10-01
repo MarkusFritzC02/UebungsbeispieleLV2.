f06817e312ad4b4d0e5e6bd3339c981ecfb717f4
Markus Fritz <52305084@edu.campus02.at>
2024-10-01 20:23:43 +0200

    Übung 3 gelöst. TurmDemo

---
+++ src/CalcFaculty.java
@@ -0,0 +1,26 @@
+public class CalcFaculty {
+    public static void main(String[] args) {
+        System.out.println(calcFaculty(0));
+        System.out.println(calcFaculty(1));
+        System.out.println(calcFaculty(2));
+        System.out.println(calcFaculty(3));
+        System.out.println(calcFaculty(4));
+        System.out.println(calcFaculty(5));
+        System.out.println(calcFaculty(11));
+        System.out.println(calcFaculty(20));
+    }
+
+    public static long calcFaculty(int value) {
+        long result = 1;
+
+        if (value == 0 || value == 1){
+            return 1;
+        }
+
+        for (int i = 2; i <= value; i++){
+            result *= i;
+        }
+        return result;
+
+    }
+}
